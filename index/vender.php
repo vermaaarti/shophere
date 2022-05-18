121700afed6b085a88c2ea8c403c9242d4f112e1
unknown <av3001560@gmail.com>
2022-05-18 13:17:25 +0530

    modified vender page

--- index/vender.php
+++ index/vender.php
@@ -8,6 +8,8 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <!--css file-->
+<link rel="stylesheet" type="text/css" href="../styling/vender.css">
     <title>vender || SHOPEO</title>
   </head>
   <body>
@@ -21,12 +23,46 @@
   <div class="col-3 m-3 rounded" style="background-color:rgb(220, 222, 220);">
     <ul class="list-unstyled" style="font-size:1.5rem;">
       <a style="text-decoration: none;" href=""><li class="m-2 ps-5">Dashboard</li></a>
+      <a style="text-decoration: none;" href=""><li class="m-2 ps-5">Products</li></a>
      <a style="text-decoration: none;" href=""><li class="m-2 ps-5">Orders</li></a>
+     <a style="text-decoration: none;" href=""><li class="m-2 ps-5">Coupons</li></a>
+     <a style="text-decoration: none;" href=""><li class="m-2 ps-5">Reviews</li></a>
+     <a style="text-decoration: none;" href=""><li class="m-2 ps-5">Withdraw</li></a>
       <a style="text-decoration: none;" href=""><li class="m-2 ps-5">Inventory</li></a>
       <a style="text-decoration: none;" href=""><li class="m-2 ps-5">Setting</li></a>
     </ul>
   </div>
-   <div class="col-9"></div>
+   <div class="col-8">
+    <div class="text-center"> <button class="btn text-light mb-5 mt-5">Add new product</button></div>
+     <div>
+      
+      <div class="d-flex">
+       <div class="dropdown">
+  <a href="#"><button class="btn btn-secondary dropdown-toggle m-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   Filter Product
+  </button></a>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">All Dates</a></li>
+    <li><a class="dropdown-item" href="#">Product Name</a></li>
+    <li><a class="dropdown-item" href="#">Product Price</a></li>
+  </ul>
+</div>
+
+   <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle m-2" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+   Search Product
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#">Product Name</a></li>
+    <li><a class="dropdown-item" href="#">Product Price</a></li>
+    <li><a class="dropdown-item" href="#">Product Category</a></li>
+    <li><a class="dropdown-item" href="#">Product By Top Rewiews</a></li>
+  </ul>
+</div>
+</div>
+
+     </div>
+   </div>
 </div>
 
 </div>
